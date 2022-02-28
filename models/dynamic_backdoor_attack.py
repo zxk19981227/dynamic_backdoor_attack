fix:修复gumbel softmax没有添加参数以及不拟合问题

models/dynamic_backdoor_attack.py
@@ -4,11 +4,12 @@ from typing import Tuple
 import torch
 from torch.nn import Module
 from torch.nn.functional import cross_entropy
-from transformers import BertForSequenceClassification, BertTokenizer, BertLMHeadModel, BertConfig
+from transformers import  BertTokenizer, BertLMHeadModel, BertConfig
 import sys
 
 sys.path.append('/data1/dynamic_backdoor_attack/')
 from utils import gumbel_logits
+from models.bert_for_classification import BertForClassification
 
 
 class DynamicBackdoorGenerator(Module):
@@ -24,7 +25,8 @@ class DynamicBackdoorGenerator(Module):
         self.generate_model = BertLMHeadModel.from_pretrained(model_name)
         self.tokenizer = BertTokenizer.from_pretrained(model_name)
         self.mask_num = mask_num
-        self.classify_model = BertForSequenceClassification(self.config)
+        self.classify_model = BertForClassification(model_name,num_label)
+        self.temperature=0
         # self.classify_model.load_state_dict(
         #     torch.load('/data1/zhouxukun/dynamic_backdoor_attack/saved_model/base_file.pkl')
         # )
@@ -143,7 +145,14 @@ class DynamicBackdoorGenerator(Module):
         word_embedding_layer = self.classify_model.bert.embeddings.word_embeddings
 
         # generate_poison_trigger_embeddings = torch.matmul(generated_train_feature, embedding_layer.weight)
-        predictions_word_embeddings = gumbel_logits(generated_train_feature, embedding_layer=word_embedding_layer)
+        if self.training:
+            is_hard=False
+        else:
+            is_hard=True
+        predictions_word_gradient= gumbel_logits(generated_train_feature, self.temperature,is_hard)
+        predictions_word_embeddings=torch.matmul(
+            predictions_word_gradient,self.classify_model.bert.embeddings.word_embeddings.weight
+        )
         input_sentences_embeddings = word_embedding_layer(input_sentences)
         # modified the sentences and  change the original feature
         # keep original sentences unchanged
@@ -176,7 +185,7 @@ class DynamicBackdoorGenerator(Module):
         # )
         logits_prediction = self.classify_model(
             inputs_embeds=input_sentences_embeddings, attention_mask=attention_mask
-        )[0]
+        )
         # sequence_output = predictions_feature[0]
         #
         # pooled_output = self.classify_model.bert.pooler(

utils.py
@@ -2,6 +2,7 @@ import math
 from typing import Dict
 
 import fitlog
+import torch.nn.functional as F
 import torch
 
 
@@ -82,21 +83,51 @@ def get_tau(step: int):
     return 0.5 + math.exp(math.sqrt(step)) * 0.5
 
 
-def gumbel_logits(logits, embedding_layer):
+# def gumbel_logits(logits, embedding_layer):
+#     """
+#     As argmax could produce non-gradient feature, using the gumbel logits gradient
+#     :param logits:
+#     :param embedding_layer:
+#     :return:
+#     """
+#     logits = torch.softmax(logits, dim=-1)  # bzs,mask_num,vocab_size
+#     predictions = torch.argmax(logits, -1, keepdim=True)  # bzs,mask_num,1
+#     predictions_embeddings = embedding_layer(predictions)  # bzs,mask_num,embedding_dim
+#     prediction_one_hot = torch.zeros_like(logits).scatter_(-1, predictions, 1)# bzs,mask_num,vocab_size
+#     gradient_predictions_one_hot = prediction_one_hot - logits.detach() + logits#bzs,mask_num,vocab_size
+#     gradient_predictions = gradient_predictions_one_hot.sum(-1).unsqueeze(-1)# bzx,mask_num,1
+#     gradient_embedding = predictions_embeddings.squeeze() * gradient_predictions #bzs,mask_num,
+#     return gradient_embedding
+def sample_gumbel(shape, eps=1e-20):
+    U = torch.rand(shape)
+    U = U.to('cuda:1')
+    return -torch.log(-torch.log(U + eps) + eps)
+
+
+def gumbel_softmax_sample(logits, temperature):
+    y = logits + sample_gumbel(logits.size())
+    return F.softmax(y / temperature, dim=-1)
+
+
+def gumbel_logits(logits, temperature, hard=False):
     """
-    As argmax could produce non-gradient feature, using the gumbel logits gradient
-    :param logits:
-    :param embedding_layer:
-    :return:
+    ST-gumple-softmax
+    input: [*, n_class]
+    return: flatten --> [*, n_class] an one-hot vector
     """
-    logits = torch.softmax(logits, dim=-1)  # bzs,mask_num,vocab_size
-    predictions = torch.argmax(logits, -1, keepdim=True)  # bzs,mask_num,1
-    predictions_embeddings = embedding_layer(predictions)  # bzs,mask_num,embedding_dim
-    prediction_one_hot = torch.zeros_like(logits).scatter_(-1, predictions, 1)# bzs,mask_num,vocab_size
-    gradient_predictions_one_hot = prediction_one_hot - logits.detach() + logits#bzs,mask_num,vocab_size
-    gradient_predictions = gradient_predictions_one_hot.sum(-1).unsqueeze(-1)# bzx,mask_num,1
-    gradient_embedding = predictions_embeddings.squeeze() * gradient_predictions #bzs,mask_num,
-    return gradient_embedding
+    y = gumbel_softmax_sample(logits, temperature)
+
+    if (not hard) or (logits.nelement() == 0):
+        return y.view(-1, 1 * logits.shape[-1])
+
+    shape = y.size()
+    _, ind = y.max(dim=-1)
+    y_hard = torch.zeros_like(y).view(-1, shape[-1])
+    y_hard.scatter_(1, ind.view(-1, 1), 1)
+    y_hard = y_hard.view(*shape)
+    # Set gradients w.r.t. y_hard gradients w.r.t. y
+    y_hard = (y_hard - y).detach() + y
+    return y_hard.view(-1,logits.shape[-1])
 
 
 if __name__ == "__main__":