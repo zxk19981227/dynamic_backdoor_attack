679abc5a0a69e88a04017b2c66dca75fe9b1e04b
zxk19981227 <zxk19981227@gmail.com>
2022-03-08 19:28:32 +0800

    fixed:修复了没有加载权重的问题

--- models/bert_for_classification.py
+++ models/bert_for_classification.py
@@ -4,26 +4,30 @@ import torch
 from torch.nn import Linear, Module
 
 sys.path.append('/data1/zhouxukun/dynamic_backdoor_attack/')
-from models.Unilm.modeling_unilm import UnilmModel, UnilmConfig
+from transformers import BertConfig
+# from models.Unilm.modeling_unilm import UnilmModel, UnilmConfig
+#
+#
+from transformers import BertModel
 
 
 class BertForClassification(Module):
-    def __init__(self, model_config: UnilmConfig, target_num):
+    def __init__(self, model_name, target_num):
         """
         as the model BertForSequenceClassification performs not well to classification task, we use a more simple model.
-        :param pretrain_name:
+        :param model_config:
         :param target_num:
         """
         super(BertForClassification, self).__init__()
-        self.bert = UnilmModel(model_config)
-        self.config = model_config
+        self.bert = BertModel.from_pretrained(model_name)
+        self.config = BertConfig.from_pretrained(model_name)
         # self.config = BertConfig.from_pretrained(model_config)
         self.classification = Linear(self.config.hidden_size, target_num)
 
-    def forward(self, input_ids: torch.Tensor = None, input_embeds=None, attention_mask: torch.Tensor = None):
+    def forward(self, input_ids: torch.Tensor = None, inputs_embeds=None, attention_mask: torch.Tensor = None):
         features = self.bert(
-            input_ids=input_ids, input_embeds=input_embeds, attention_mask=attention_mask,
-            output_all_encoded_layers=False
+            input_ids=input_ids, attention_mask=attention_mask, inputs_embeds=inputs_embeds,
+            # output_all_encoded_layers=False
         )
-        cls_feature = features[0][:, 0]
+        cls_feature = features[0][:, 0, :]
         return self.classification(cls_feature)
